860f10bc159efbcdc22b4797892064cf18ff07a0
Chris <chris@redbeed.com>
2020-12-23 16:57:26 +0100

    Fix grant type

--- src/Http/Controllers/Connection/AppTokenController.php
+++ src/Http/Controllers/Connection/AppTokenController.php
@@ -2,10 +2,10 @@
 
 namespace Redbeed\OpenOverlay\Http\Controllers\Connection;
 
-use Redbeed\OpenOverlay\Http\Controllers\Connection\SocialiteController;
-
 class AppTokenController extends SocialiteController
 {
+
+    protected $socialiteDriver = 'twitch_client_credentials';
 
     public function __construct()
     {
