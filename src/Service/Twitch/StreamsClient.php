36981e41942bc458ec7d64ac7bbf7bfd6dffda2f
Chris Woelk <chris@redbeed.com>
2022-01-20 09:53:26 +0100

    - Add helper functions for StreamsClient.php

--- src/Service/Twitch/StreamsClient.php
+++ src/Service/Twitch/StreamsClient.php
@@ -8,11 +8,37 @@ class StreamsClient extends ApiClient
 {
     public function byUserId(string $userId): array
     {
+        return $this->byUserIds([$userId]);
+    }
+
+    public function byUsername(string $username): array
+    {
+        return $this->byUsernames([$username]);
+    }
+
+    public function byUserIds(array $userIds): array
+    {
         return $this
             ->addAppToken()
             ->withOptions([
                 RequestOptions::QUERY => [
-                    'user_id' => $userId,
+                    'user_id' => array_map(function ($userId) {
+                        return intval($userId);
+                    }, $userIds),
+                ],
+            ])
+            ->request('GET', 'streams');
+    }
+
+    public function byUsernames(array $usernames): array
+    {
+        return $this
+            ->addAppToken()
+            ->withOptions([
+                RequestOptions::QUERY => [
+                    'user_login' => array_map(function ($username) {
+                        return strtolower($username);
+                    }, $usernames),
                 ],
             ])
             ->request('GET', 'streams');
